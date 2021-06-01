38d1203d1fc60ddbb87df0de2b37660473a08467
JPGDubois <79090591+JPGDubois@users.noreply.github.com>
2021-06-01 23:00:14 +0200

    first test version path with comments

--- path.py
+++ path.py
@@ -1,6 +1,7 @@
 import numpy as np
-import math
 import foil_refine
+import pandas as pd
+
 """
 Current approach:
     cut top and bottom surface from TE to LE.
@@ -26,34 +27,35 @@ Current approach:
         -> cut upper surface
         -> cut lead out in LE
 Output different lists for each step,
-each starting with a header containing the name and type(cut/rapid)
 """
-
 
 class wing_path:
     def __init__(self, name, coords, roof):
-        self.name = str(name)
-        self.coords = coords
-        self.roof = 0
-        self.leadin_len = 0
-        self.leadout_len = 0
-        self.o = [0, 0, 0, 0]
+        # Parameters for the path generation
+        self.name = str(name)   # gcode name
+        self.coords = coords    # input lists with foil points
+        self.roof = 0           # height at which rapids are performed
+        self.leadin_len = 0     # lenght of lead in at TE
+        #self.leadout_len = 0    # lenght of lead out at LE
+        self.o = [0, 0, 0, 0]   # gcode origen
 
-        self.lu = []    # [[x, y], ...]
-        self.ll = []    # [x, y]
-        self.ru = []    # [z, u]
-        self.rl = []    # [z, u]
+        # Splitted airfoil coordinates
+        self.lu = []    # [[x, y], ...] left upper
+        self.ll = []    # [x, y], ...] left lower
+        self.ru = []    # [z, u], ...] right upper
+        self.rl = []    # [z, u], ...] right lower
 
-        self.gcode = []
-        self.ax1 = 'X'
-        self.ax2 = 'Y'
-        self.ax3 = 'U'
-        self.ax4 = 'Z'
-        self.M = [300, 500]
-        self.F = [100, 250]
+        # Parameters for gcode generation
+        self.gcode = [] # gcode commands
+        self.ax1 = 'X'  # axis name first axis
+        self.ax2 = 'Y'  # axis name second axis
+        self.ax3 = 'U'  # axis name third axis
+        self.ax4 = 'Z'  # axis name fourth axis
+        self.M = [300, 500] # cutting temperature range
+        self.F = [100, 250] # cutting feed rate range
 
     def split():
-        # do stuf to get self.lu,ll,ru,rl
+        # Split coords into different section; left upper, left lower, right upper and right cut_lower
         arr = self.Coords.get_coordinates()
 
         self.lu = [ arr[0, 0, :-(-len(arr[0, 0])//2)+1, arr[0, 1, :-(-len(arr[0, 1])//2)+1 ]
@@ -62,35 +64,44 @@ class wing_path:
         self.rl = [ arr[1, 0, -(-len(arr[1, 0])//2):, arr[1, 1, -(-len(arr[1, 1])//2): ]
         return
 
-    def rapid_upper(self):    # start, end [x, y, z, u]
+    def rapid_upper(self):
+        # Moves is a rectangular path to the start of the upper cut,
+        # the height of the cut is determined by roof.
         start = self.o
         end = [self.lu[0]+self.leadin_len, self.lu[1], self.ru[0]+self.leadin_len, self.ru[1]]
         path = [[start[0], self.roof, start[2], self.roof], [end[0], self.roof, end[2], self.roof], end]
         return path
 
-    def rapid_lower(self):    # start, end [x, y, z, u]
+    def rapid_lower(self):
+        # Moves is a rectangular path to the start of the lower cut,
+        # the height of the cut is determined by roof.
         start = self.o
         end = [self.ll[0]+self.leadin_len, self.ll[1], self.rl[0]+self.leadin_len, self.rl[1]]
         path = [[start[0], self.roof, start[2], self.roof], [end[0], self.roof, end[2], self.roof], end]
         return path
 
     def cut_upper(self):
+        # Do a straight lead in, trace the airfoil upper path
+        # and do the lead out tangential to the airfoil LE.
         path = []
         for i in range(len(self.lu)):
             path.append(self.lu[i] + self.ru[i])
-        path.append(self.rotate(path[-1], 5, -math.pi/2))
+        path.append(self.rotate(path[-1], 5, -np.pi/2))
         path.append([self.o[0], path[-1]])
         return path
 
     def cut_lower(self):
+        # Do a straight lead in, trace the airfoil lower path
+        # and do the lead out tangential to the airfoil LE.
         path = []
         for i in range(len(self.ll)):
             path.append(self.ll[i] + self.rl[i])
-        path.append(self.rotate(path[-1], 5, math.pi/2))
+        path.append(self.rotate(path[-1], 5, np.pi/2))
         path.append([self.o[0], path[-1]])
         return path
 
     def rotate(self, start, R, endangle):
+        # Make a list of points on a circular path used for the tangential leadout.
         oxl = start[0] - R
         oyl = start[1]
         oxr = start[2] - R
@@ -98,23 +109,29 @@ class wing_path:
         angle = 0
         path = []
         while angle >= endangle:
-            pathl = [oxl + math.cos(-angle) * (start[0] - oxl) - math.sin(-angle) * (start[1] - oyl), oyl + math.sin(-angle) * (start[0] - oxl) + math.cos(-angle) * (start[1] - oyl)]
-            pathr = [oxr + math.cos(-angle) * (start[2] - oxr) - math.sin(-angle) * (start[3] - oyr), oyr + math.sin(-angle) * (start[2] - oxr) + math.cos(-angle) * (start[3] - oyr)]
+            pathl = [oxl + np.cos(-angle) * (start[0] - oxl) - np.sin(-angle) * (start[1] - oyl), oyl + np.sin(-angle) * (start[0] - oxl) + np.cos(-angle) * (start[1] - oyl)]
+            pathr = [oxr + np.cos(-angle) * (start[2] - oxr) - np.sin(-angle) * (start[3] - oyr), oyr + np.sin(-angle) * (start[2] - oxr) + np.cos(-angle) * (start[3] - oyr)]
             path.append(pathl + pathr)
-            angle -= math.pi/40
+            angle -= np.pi/40
         return path
 
     def home(self):
+        # Return the home location
         return self.o
 
     def G0(self, path):
+        # Convert the rapid path to machine code
         self.gcode.append('G0')
+
         for i in path:
             line = str(self.ax1+str(i[0])+ ' ' +self.ax2+str(i[1])+ ' ' + self.ax3+str(i[2])+' '+ self.ax4+str(i[3]))
             self.gcode.append(line)
         return self.gcode
 
     def G1(self, path, F, M):
+        # Convert the cutting path to machine code with feed rate modulation
+        #
+        # Determine the feed rate and cutting temperature based on the point density
 
         # Modify feedrate depending on point density, only one side is considered since they are equivalent.
         dl = [np.sqrt((path[i+1, 0] - path[i, 0])**2+(path[i+1, 1] - path[i, 1])**2) for i in range(len(path)-1)]
@@ -134,10 +151,17 @@ class wing_path:
             self.gcode.append(line)
         return self.gcode
 
-    def Start(self):
-        self.gcode.append('%','('+self.name+') G21 G90 M5')
+    def start(self):
+        # Return the starting lines of machine code
+        self.gcode.extend(['%','('+self.name+') G21 G90 M5'])
         return self.gcode
 
     def end(self):
-        self.gcode.append('M5','%')
+        # Return the ending lines of gcode
+        self.gcode.extend(['M5','%'])
         return self.gcode
+
+    def export(self):
+        # Write a txt file containing the machine code
+        df = pd.DataFrame(salary)
+        df.to_csv(str(self.name) + '.csv', index=False, header=False)
