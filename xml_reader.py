d90797feb6a98f747a118c9ddee9c33cc2e71928
JPGDubois <79090591+JPGDubois@users.noreply.github.com>
2021-09-03 18:53:11 +0200

    Start on import option for XFLR5

--- xml_reader.py
+++ xml_reader.py
@@ -1,4 +1,27 @@
-import pandas as pd
+file = "Main_Wing.xml"
 
-df = pd.read_xml('Main_Wing.xml')
-print(df)
+f = open(file, "r")
+lines = f.readlines()
+f.close()
+
+sectionAmount = 0
+
+for line in lines:
+    sectionAmount += line.count("<Section>")
+print(sectionAmount)
+class Section:
+    def __init__(self, sectionNr):
+        self.sectionNr = sectionNr
+        self.dict = {}
+
+
+
+
+
+
+
+
+
+
+Section1 = Section(4)
+print(Section1.dict)
