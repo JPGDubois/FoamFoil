04fcade5359343df68262c0751d471838a098859
alpha-v <alphaverta@gmail.com>
2022-02-19 23:57:24 +0100

    Add plane to help orientate oneself + basic and shitty keyboard control

--- opengl.py
+++ opengl.py
@@ -29,6 +29,12 @@ void main()
     gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
 } """
 
+plane_fragment = """
+void main()
+{
+    gl_FragColor = vec4(1.0, 0.0, 1.0, 0.3);
+} """
+
 #root = np.array([[0, 0, 0], [100, 100, 100]])
 
 V = np.zeros(root.shape[0] + tip.shape[0], [("a_position", np.float32, 3)])
@@ -71,6 +77,15 @@ I = I.view(gloo.IndexBuffer)
 cube = gloo.Program(vertex, fragment)
 cube["a_position"] = V
 
+# Draw some form of ground / a plane to orient ourselves
+V_plane = np.zeros(4, [("a_position", np.float32, 3)])
+V_plane["a_position"] = np.array([[0, 0, 0], [0, 0, 1], [1, 0, 1], [1, 0, 0]]) * 2 + np.array([0, 0, -2]) - root[0] / 100
+I_plane = np.array([0, 1, 2,  0, 3, 2], dtype=np.uint32)
+V_plane = V_plane.view(gloo.VertexBuffer)
+I_plane = I_plane.view(gloo.IndexBuffer)
+plane = gloo.Program(vertex, plane_fragment)
+plane["a_position"] = V_plane
+
 view = np.eye(4,dtype=np.float32)
 model = np.eye(4,dtype=np.float32)
 projection = np.eye(4,dtype=np.float32)
@@ -78,12 +93,18 @@ glm.translate(view, 0,0,-5)
 cube['u_model'] = model
 cube['u_view'] = view
 cube['u_projection'] = projection
-phi, theta = 0,0
+
+plane['u_model'] = model
+plane['u_view'] = view
+plane['u_projection'] = projection
+
+phi, theta = 70, 55
 
 @window.event
 def on_resize(width, height):
    ratio = width / float(height)
    cube['u_projection'] = glm.perspective(45.0, ratio, 2.0, 100.0)
+   plane['u_projection'] = glm.perspective(45.0, ratio, 2.0, 100.0)
 
 
 draw_time = 5  # s
@@ -93,20 +114,36 @@ def on_draw(dt):
     global phi, theta, cur_time
     window.clear()
     cube.draw(gl.GL_LINES, I[:math.ceil((cur_time % draw_time) / draw_time * len(I))])
-    print(math.ceil((cur_time % draw_time) / draw_time) * len(I))
+
+    plane.draw(gl.GL_TRIANGLES, I_plane)
 
     # Make cube rotate
-    theta += 30 * dt # degrees
-    phi += 30 * dt # degrees
+    #theta += 30 * dt # degrees
+    #phi += 30 * dt # degrees
     cur_time += dt
     model = np.eye(4, dtype=np.float32)
     glm.rotate(model, theta, 0, 0, 1)
     glm.rotate(model, phi, 0, 1, 0)
     cube['u_model'] = model
+    plane['u_model'] = model
 
 @window.event
 def on_init():
     gl.glEnable(gl.GL_DEPTH_TEST)
+
+# Hoenstly guessing the variables here
+@window.event
+def on_key_press(keycode, something_else):
+    global phi, theta
+    print(keycode)
+    if keycode == 65363:
+        phi += 1
+    elif keycode == 65361:
+        phi -= 1
+    elif keycode == 65362:
+        theta += 1
+    elif keycode == 65364:
+        theta -= 1
 
 # Run the app
 app.run()
