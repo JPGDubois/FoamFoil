ee0946dc8318d6e4fb66d076efcf7dd9ac94e465
alpha-v <alphaverta@gmail.com>
2022-02-20 12:18:41 +0100

    First run at cleaning up glumpy demo code

--- opengl.py
+++ opengl.py
@@ -9,10 +9,10 @@ coordinate_dump = np.load("coordinate_dump.npz")
 root = coordinate_dump['root'].T
 tip = coordinate_dump['tip'].T
 
-print(root)
 
 window = app.Window()
 
+# Set up shader code that enables us to use projection on vertices
 vertex = """
 uniform mat4   u_model;         // Model matrix
 uniform mat4   u_view;          // View matrix
@@ -23,61 +23,50 @@ void main()
     gl_Position = u_projection * u_view * u_model * vec4(a_position,1.0);
 } """
 
+# Shader code that specified the color for the lines that make up the wing wireframe
 fragment = """
 void main()
 {
     gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
 } """
 
+# Shader code that specifies the color for the reference plane
 plane_fragment = """
 void main()
 {
     gl_FragColor = vec4(1.0, 0.0, 1.0, 0.3);
 } """
 
-#root = np.array([[0, 0, 0], [100, 100, 100]])
-
+# Load in all airfoil points/vertices
 V = np.zeros(root.shape[0] + tip.shape[0], [("a_position", np.float32, 3)])
 # Scale by a factor of 100, which seems to somewhat normalize it to what fits on a screen. Also offset it to try
 # to get the center of rotation to be somewhat in the middle (I can probably do an exact solution, but laziness you
 # know)
 V["a_position"] = (np.vstack((root, tip)) - np.array([50, 200, 50])) / 100
 
-# Draw the lines
-# NOTE: Assumes number of tip/root datapoints are the same
+# Connect the airfoil segments using straight lines
+# NOTE: Assumes number of tip/root data points are the same
 I_list = []
 for i in range(0, root.shape[0]):
     # Connect the airfoil segments themselves
     I_list.append([i, (i + 1) % (root.shape[0] - 1)])
     I_list.append([i + root.shape[0], (i + root.shape[0] + 1) % (root.shape[0] + tip.shape[0] - 1)])
 
-    # Connect two airfoils (but only do this for some datapoints to avoid weird rendering effects)
+    # Connect two airfoils (but only do this for some data points to avoid weird rendering effects due to a large
+    # number of lines)
     if i % 5 == 0:
         I_list.append([i, i + root.shape[0]])
+# Convert the line connections into a numpy array; we could technically calculate the size, but sinced this is a one-off
+# process I'm not too worries about efficiency
 I = np.array(I_list, dtype=np.uint32)
-print(V)
-print(I)
 
-print("=====")
-print(tip)
-print("!!!!!")
-print(root)
-
-#V["a_position"] = [[ 1, 1, 1], [-1, 1, 1], [-1,-1, 1], [ 1,-1, 1],
-#                   [ 1,-1,-1], [ 1, 1,-1], [-1, 1,-1], [-1,-1,-1]]
-#I = np.array([0,1,2, 0,2,3,  0,3,4, 0,4,5,  0,5,6, 0,6,1,
-#              1,6,7, 1,7,2,  7,4,3, 7,3,2,  4,7,6, 4,6,5], dtype=np.uint32)
-
-#I = np.array([0,1,2, 0,2,3,  0,3,4, 0,4,5,  0,5,6, 0,6,1,
-#              1,6,7, 1,7,2,  7,4,3, 7,3,2,  4,7,6, 4,6,5], dtype=np.uint32)
-
+# Send the wing vertex/index buffer information off to the rendering engine so we can draw it to the screen later
 V = V.view(gloo.VertexBuffer)
 I = I.view(gloo.IndexBuffer)
-
 cube = gloo.Program(vertex, fragment)
 cube["a_position"] = V
 
-# Draw some form of ground / a plane to orient ourselves
+# Now create a simple plane located roughly at the root of the wing so we have a sense of direction when panning around
 V_plane = np.zeros(4, [("a_position", np.float32, 3)])
 V_plane["a_position"] = np.array([[0, 0, 0], [0, 0, 1], [1, 0, 1], [1, 0, 0]]) * 2 + np.array([0, 0, -2]) - root[0] / 100
 I_plane = np.array([0, 1, 2,  0, 3, 2], dtype=np.uint32)
@@ -100,11 +89,12 @@ plane['u_projection'] = projection
 
 phi, theta = 70, 55
 
+
 @window.event
 def on_resize(width, height):
-   ratio = width / float(height)
-   cube['u_projection'] = glm.perspective(45.0, ratio, 2.0, 100.0)
-   plane['u_projection'] = glm.perspective(45.0, ratio, 2.0, 100.0)
+    ratio = width / float(height)
+    cube['u_projection'] = glm.perspective(45.0, ratio, 2.0, 100.0)
+    plane['u_projection'] = glm.perspective(45.0, ratio, 2.0, 100.0)
 
 
 draw_time = 5  # s
@@ -112,14 +102,15 @@ cur_time = 0  # s; total elapsed time
 @window.event
 def on_draw(dt):
     global phi, theta, cur_time
+
+    # Clear the window and draw the wing, reference plane.
+    # NOTE: We're gradually showing nore of the airfoil, which is why we only select some of the points. This does
+    # assume that there is some sense of order to the points / they form a kind of sequence
     window.clear()
     cube.draw(gl.GL_LINES, I[:math.ceil((cur_time % draw_time) / draw_time * len(I))])
-
     plane.draw(gl.GL_TRIANGLES, I_plane)
 
-    # Make cube rotate
-    #theta += 30 * dt # degrees
-    #phi += 30 * dt # degrees
+    # Increase the time counter and update the projection matrices to match the requested pan angles
     cur_time += dt
     model = np.eye(4, dtype=np.float32)
     glm.rotate(model, theta, 0, 0, 1)
@@ -127,11 +118,14 @@ def on_draw(dt):
     cube['u_model'] = model
     plane['u_model'] = model
 
+
 @window.event
 def on_init():
     gl.glEnable(gl.GL_DEPTH_TEST)
 
-# Hoenstly guessing the variables here
+
+# Keyboard input handlers. They do not appear to create smooth movement, but allow basic panning using the arrow keys
+# NOTE: Unable to find a whole lot of documentation; guessing the variables at this point
 @window.event
 def on_key_press(keycode, something_else):
     global phi, theta
@@ -144,6 +138,7 @@ def on_key_press(keycode, something_else):
         theta += 1
     elif keycode == 65364:
         theta -= 1
+
 
 # Run the app
 app.run()
